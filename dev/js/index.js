ea9cb49c34cb8844d3d2e45f08214e788a9088f1
ailer8469 <ailer8469@gmail.com>
2021-11-08 18:03:48 +0800

    11/8

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,6 +1,36 @@
 $(document).ready(function(){
-    
+    //open copy phone
+    $('#link_phone').on('click',function(){
+        $('.poptext').fadeToggle(600)
+    });
+    //copy phone number
+    $('#popnumber').click(function(){
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($('#copyPhone').text()).select();
+        document.execCommand("copy");
+        $temp.remove();
 
+        let modal = document.getElementById("alert_modal");
+        let span = document.getElementsByClassName("close")[0];
+        modal.style.display = "block";
+
+        span.onclick = function() {
+            modal.style.display = "none";
+            }
+        window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            }
+        }
+        window.ontouchend = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    });
+
+    //open cooperate modal
     let modal=$('.cooperate_modal');
     let open_modal=$('.cooperate_item');
     let close_modal =$(".modal_close");
@@ -8,20 +38,29 @@ $(document).ready(function(){
         modal.css({
             'display':"flex",
         })
-    })
+    });
     close_modal.on('click',function(){
         modal.css({
             'display':"none",
         })
-    })
+    });
     $(window).click(function(event){
         if(event.target.className == 'cooperate_modal'){
             modal.css({
                 'display':"none",
             })
         }
-    })    
+    });  
+    //hover animation
+    $('.map_box.ask').hover(function(){
+        $('.pig_map_off').hide();
+        $('.pig_map_on').show();
+    },function(){
+        $('.pig_map_off').show();
+        $('.pig_map_on').hide();
+    })
 });
+//cloud parallax
 const wrap=document.getElementById('top_cloud_wrap');
 const scene = new Parallax(wrap,{
     scalarX: 10,
