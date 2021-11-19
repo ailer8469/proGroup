87c3881aaec92f74df0558bb95136600f222306d
ailer8469 <ailer8469@gmail.com>
2021-11-19 18:04:53 +0800

    11/19

--- dev/js/layout/loading.js
+++ dev/js/layout/loading.js
@@ -1,11 +1,37 @@
+const load = lottie.loadAnimation({
+    container: document.getElementById("loaded"), 
+    loop: false,
+    autoplay: true,
+    path: "https://assets2.lottiefiles.com/packages/lf20_wz1jiozw.json"
+});
+
 $(function(){
+    circle= $('#loaded');
     $('body').waitForImages({
         finished: function() {
-            $('#loadpage').addClass('loading-done');
+            $('main').css('display', 'none');
+            // animation up
+            load.playSegments([250,360], true);
+            $('#progress').fadeOut(1000);
             setTimeout(function(){
-                $('#loadpage').slideToggle(500);
-            }, 1500);
-            $('.section').css('display', 'flex');
+                let tl = new TimelineMax();
+                tl.to($('#mask_circle'), {
+                    opacity:1
+                }).to(circle,{
+                    opacity:0
+                }).to($('#mask_circle'),1,{
+                    scale:20,
+                    ease: "power1.out",
+                }).to($('#loadpage'),{
+                    'display':'none',
+                }).to($('#mask'),1,{
+                    scale:20,
+                    ease: "power1.out",
+                }).to($('#mask_circle'),{
+                    'display':'none',
+                });
+            }, 1000);
+            $('main').css('display', 'block');
         },
         each: function(loaded, count, success) {
             var pNum = parseInt((loaded / count) * 100);
--- dev/js/member.js
+++ dev/js/member.js
@@ -5,4 +5,16 @@ $(function(){
     },function(){
         $('#arrow_right').removeClass('active');
     });
+    $('#arrow_left').hover(function(){
+        $('#arrow_left').addClass('active');
+    },function(){
+        $('#arrow_left').removeClass('active');
+    });
+
+    //change title
+    let index= $('.member_slider.slick-current').index();
+    $('.main_slider').on('afterChange', function(){
+        // load item number
+        $('.title_item').addClass('memactive');
+    });
 });
